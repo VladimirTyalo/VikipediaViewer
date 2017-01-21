93617d264ea4dc48a276a8b39c74d36f20ccd301
Vladimir Tyalo <bobtial@inbox.ru>
2017-01-21 21:30:49 +0400

    chore(webpack) add styles and autoprefixer

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import styles from './App.css';
+
 export class App extends Component {
     render() {
         return (
             <div className='app'>
                 Wikipedia Viewer
                 <div>Some description goes here</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolores.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolores.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolores.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, dolores.</p>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-    entry: ['./src/index.js'],
+const PATHS = {
+    app: path.join(__dirname, 'src/index.js'),
+    dist: path.join(__dirname, 'dist'),
+    src: path.join(__dirname, 'src')
+};
+
+// ---------- Common (development/production) webpack configuration -----------
+const common = {
+    entry: {
+        app: PATHS.app
+    },
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'build.js',
+        path: PATHS.dist,
+        filename: '[name].js',
         publicPath: '/dist/'
     },
     resolve: {
         // for webpack version < 2.0 use ['', '.js', '.jsx']
-        extensions: ['*', '.js', '.jsx']
+        extensions: ['*', '.js', '.jsx', '.css']
     },
+    devtool: 'eval-source-map',
     module: {
 
         loaders: [
@@ -21,7 +34,69 @@ module.exports = {
                 query: {
                     presets: ['react', 'es2015', 'stage-0']
                 }
+            }, {
+                test: /\.css$/,
+                loaders: [
+                    'style-loader', 'css-loader?sourceMap', 'autoprefixer-loader?browsers=Last 3 versions'
+                ]
             }
         ]
+    },
+    plugins: [new HtmlWebpackPlugin({title: 'Wikipedia Viewer', filename: 'index.html', template: 'src/templates/index.html', favicon: 'src/assets/images/favicons/favicon.ico'})]
+};
+
+const developmentConfig = {
+    devServer: {
+        // Enable history API fallback so HTML5 History API based routing works. This is
+        // a good default that will come in handy in more complicated setups.
+        historyApiFallback: true,
+
+        // Unlike the cli flag, this doesn't set HotModuleReplacementPlugin!
+        hot: true,
+
+        inline: true,
+
+        // Display only errors to reduce the amount of output.
+        stats: 'errors-only',
+        // Parse host and port from env to allow customization.
+        //
+        // If you use Vagrant or Cloud9, set host: options.host || '0.0.0.0';
+        //
+        // 0.0.0.0 is available to all network devices unlike default `localhost`.
+        host: process.env.HOST, // Defaults to `localhost`
+        port: process.env.PORT, // Defaults to 8080
+
+        watchOptions: {
+            // Delay the rebuild after the first change
+            aggregateTimeout: 300,
+            // Poll using interval (in ms, accepts boolean too)
+            poll: 1000
+        }
+    },
+    plugins: [
+        // Enable multi-pass compilation for enhanced performance in larger projects.
+        // Good default.
+        new webpack.HotModuleReplacementPlugin({
+            // Disabled as this won't work with html-webpack-template yet multiStep: true,
+        }),
+        new webpack.NamedModulesPlugin(),
+        // ignore node_modules so CPU usage with poll watching drops significantly
+        new webpack.WatchIgnorePlugin([path.join(__dirname, 'node_modules')])
+    ]
+};
+
+module.exports = function webpackConfig(env) {
+    console.log(`env: ${env}`);
+
+    if (env.toLowerCase() == 'production') {
+        return common;
     }
+
+    const dev = Object.assign({}, common, developmentConfig, {
+        plugins: common
+            .plugins
+            .concat(developmentConfig.plugins)
+    });
+
+    return dev;
 };
